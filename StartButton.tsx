ae94ef133e62332f7bfc2c4f3e2d0ef3c184cbc0
robby <robbybruin@g.ucla.edu>
2024-03-12 14:58:47 -0700

    fix: beautify

--- StartButton.tsx
+++ StartButton.tsx
@@ -17,11 +17,11 @@ export function StartButton() {
   }, [])
 
   return (
-    <div className="flex justify-center my-5">
+    <div className="my-5 flex justify-center">
       <button
         onClick={toggle}
         onTouchStart={toggle}
-        className="group flex items-center gap-1 text-sm tracking-[.2em] py-2 px-4 rounded-full bg-gradient-to-r from-blue-500 to-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:to-blue-500 transition-colors duration-300 ease-in-out active:bg-pink-500 focus:outline-none scale-110 transform transition-transform duration-200 text-white font-semibold"
+        className="group flex scale-110 transform items-center gap-1 rounded-full bg-gradient-to-r from-blue-500 to-pink-500 px-4 py-2 text-sm font-semibold tracking-[.2em] text-white transition-colors transition-transform duration-200 duration-300 ease-in-out hover:bg-gradient-to-r hover:from-pink-500 hover:to-blue-500 focus:outline-none active:bg-pink-500"
       >
         <span className="hidden md:inline">Begin Your</span>
         <kbd className="keyboard hidden tracking-normal opacity-90 md:inline">
