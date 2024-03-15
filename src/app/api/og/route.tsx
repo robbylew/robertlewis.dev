4b104e1057a2c30ad0f8ce702d87db19ffadf41e
Robert Lewis <robraylew@gmail.com>
2024-03-15 03:24:56 -0700

    fix: changed memoji

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,7 +1,7 @@
 export async function GET() {
   try {
     const response = await fetch(
-      'https://og.anuragroy.dev/api?theme=blue&title=Robert+Lewis+&description=Aspiring+Software+Developer.+Come+check+out+my+portfolio.&avatar=https://www.robertlewis.dev/assets/memoji.jpg&author=robertlewis.dev&logo=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F82246610%3Fv%3D4'
+      'https://og.anuragroy.dev/api?theme=blue&title=Robert+Lewis+&description=Aspiring+Software+Developer.+Come+check+out+my+portfolio.&avatar=https://www.robertlewis.dev/assets/memoji.jpg&author=robertlewis.dev&logo='
     )
     const data = await response.blob()
     return new Response(data, {
