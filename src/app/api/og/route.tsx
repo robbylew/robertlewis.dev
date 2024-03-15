8c77003d1e8b6123db7f4eebea2aaa6355598c97
Robert Lewis <robraylew@gmail.com>
2024-03-15 03:43:50 -0700

    fix: changed color to gray; route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,7 +1,7 @@
 export async function GET() {
   try {
     const response = await fetch(
-      'https://og.anuragroy.dev/api?theme=blue&title=Robert+Lewis+&description=Aspiring+Software+Developer.+Come+check+out+my+portfolio.&avatar=https://www.robertlewis.dev/assets/memoji.jpg&author=robertlewis.dev&logo='
+      'https://og.anuragroy.dev/api?theme=gray&title=Robert+Lewis+&description=Aspiring+Software+Developer.+Come+check+out+my+portfolio.&avatar=https://www.robertlewis.dev/assets/memoji.jpg&author=robertlewis.dev&logo='
     )
     const data = await response.blob()
     return new Response(data, {
