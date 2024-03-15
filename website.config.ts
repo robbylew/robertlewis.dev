c4615a6873c372f4a6116751ff4289ab74310002
Robert Lewis <robraylew@gmail.com>
2024-03-15 01:11:59 -0700

    fix: changed api image

--- website.config.ts
+++ website.config.ts
@@ -2,9 +2,9 @@ export const config = {
   metadata: {
     title: 'Robert Lewis',
     description:
-      'Robert Lewis - Aspiring Software Developer specializing in modern web technologies. Check out my portfolio to see my projects and skills.'
+      'Robert Lewis - Aspiring Software Developer. Check out my portfolio to see my projects and skills.'
   },
   webserver: {
-    host: process.env.HOST ?? 'http://localhost:3000'
+    host: process.env.HOST ?? 'https://www.robertlewis.dev'
   }
 }
