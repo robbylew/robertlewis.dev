bf1e58ba8f8d7da12892652b8937e1f030a385a1
Robert Lewis <robraylew@gmail.com>
2024-03-15 02:56:13 -0700

    fix: final api change for the night

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,75 +1,12 @@
-import { ImageResponse } from 'next/og'
-// App router includes @vercel/og.
-// No need to install it.
-
-export const runtime = 'edge'
-
 export async function GET() {
   try {
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            display: 'flex',
-            height: '100%',
-            width: '100%',
-            alignItems: 'center',
-            justifyContent: 'center',
-            letterSpacing: '-.02em',
-            fontWeight: 700,
-            background: 'white'
-          }}
-        >
-          <div
-            style={{
-              left: 42,
-              top: 42,
-              position: 'absolute',
-              display: 'flex',
-              alignItems: 'center'
-            }}
-          >
-            <span
-              style={{
-                width: 24,
-                height: 24,
-                background: 'black'
-              }}
-            />
-            <span
-              style={{
-                marginLeft: 8,
-                fontSize: 20
-              }}
-            >
-              robertlewis.dev
-            </span>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              justifyContent: 'center',
-              padding: '20px 50px',
-              margin: '0 42px',
-              fontSize: 45,
-              width: 'auto',
-              maxWidth: 550,
-              textAlign: 'center',
-              backgroundColor: 'black',
-              color: 'white',
-              lineHeight: 1.4
-            }}
-          >
-            Robert Lewis
-          </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630
-      }
+    const response = await fetch(
+      'https://og.anuragroy.dev/api?theme=blue&title=Robert+Lewis+&description=Aspiring+Software+Developer.+Come+check+out+my+portfolio.&avatar=https://www.robertlewis.dev/assets/memoji.jpg&author=robertlewis.dev&logo=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F82246610%3Fv%3D4'
     )
+    const data = await response.blob()
+    return new Response(data, {
+      headers: { 'Content-Type': 'image/png' }
+    })
   } catch (e: any) {
     console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
